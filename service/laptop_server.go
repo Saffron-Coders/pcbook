8cbcdd00dc255e40bf7b9dc126b4c1355cb8d0db
Dave Augustus <davetweetlive@gmail.com>
2022-08-30 23:30:40 +0530

    UNARY RPC DONE
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

---
+++ service/laptop_server.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/google/uuid"
+	"gitlab.techschool/pcbook/pb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type LaptopServer struct {
+	Store LaptopStore
+}
+
+func NewLaptopServer() *LaptopServer {
+	return &LaptopServer{}
+}
+
+func (server *LaptopServer) CreateLaptop(ctx context.Context, req *pb.CreateLaptopRequest) (*pb.CreateLaptopResponse, error) {
+	laptop := req.GetLaptop()
+	log.Printf("received a create-laptop request with id: %s", laptop.Id)
+
+	if len(laptop.Id) > 0 {
+		// check if it's a valid UUID
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop id is not a valid UUID: %v", err)
+		} else {
+			id, err := uuid.NewRandom()
+			if err != nil {
+				return nil, status.Errorf(codes.Internal, "cannot generate a new UUID: %v", err)
+			}
+			laptop.Id = id.String()
+		}
+	}
+
+	// Save the laptop to in
+	err := server.Store.Save(laptop)
+	if err != nil {
+		code := codes.Internal
+		if errors.Is(err, ErrAlreadyExists) {
+			code = codes.AlreadyExists
+		}
+		return nil, status.Errorf(code, "cannot save laptop to the store: %v", err)
+	}
+
+	log.Printf("saved laptop with id: %s", laptop.Id)
+
+	res := &pb.CreateLaptopResponse{
+		Id: laptop.Id,
+	}
+	return res, nil
+}
---
+++ service/laptop_store.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+
+	"github.com/jinzhu/copier"
+	"gitlab.techschool/pcbook/pb"
+)
+
+var ErrAlreadyExists = errors.New("record already exists")
+
+type LaptopStore interface {
+	Save(laptop *pb.Laptop) error
+}
+
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	data  map[string]*pb.Laptop
+}
+
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data: make(map[string]*pb.Laptop),
+	}
+}
+
+func (store InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	// deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return fmt.Errorf("cannot copy laptop: %w", err)
+	}
+
+	store.data[other.Id] = other
+	return nil
+}
